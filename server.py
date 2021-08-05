6b19cfd59a2a43c11dda837c2bf959546a1f1573
anousoneFS <sone.freestyle7898@gmail.com>
2021-08-05 15:12:47 +0700

    30

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb_user_blog.file"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
 
 #config sqlite3 to enforce foreign key constrains
@@ -14,7 +14,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
 def _set_sqlite_pragma(dbapi_connection, connection_record):
     if isinstance(dbapi_connection, SQLite3Connection):
         cursor = dbapi_connection.cursor()
-        cursor.executed("PRAGMA_foreign_key=ON;")
+        cursor.execute("PRAGMA foreign_keys=ON;")
         cursor.close()
 
 db = SQLAlchemy(app)
@@ -29,3 +29,52 @@ class User(db.Model):
     address = db.Column(db.String(200))
     phone = db.Column(db.String(50))
     posts = db.relationship("BlogPost")
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(200))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+
+app.route("/user", methods=["POST"])
+def create_user():
+    pass
+
+app.route("/user/descending_id", methods=["GET"])
+def get_all_users_descending():
+    pass
+
+app.route("/user/ascending_id", methods=["GET"])
+def get_all_users_ascending():
+    pass
+
+app.route("/user/<user_id>", methods=["GET"])
+def get_one_user(user_id):
+    pass
+
+app.route("/user/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    pass
+
+app.route("/blog_post/<user_id>", methods=["POST"])
+def create_blog_post(user_id):
+    pass
+
+app.route("/user/<user_id>", methods=["GET"])
+def get_all_blog_posts(user_id):
+    pass
+
+app.route("/blog_post/<blog_post_id>", methods=["GET"])
+def get_one_blog_post(blog_post_id):
+    pass
+
+app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
+def delete_blog_post(blog_post_id):
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
