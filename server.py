2ca2b1fcc045113e35d8125883cee4428f751605
anousoneFS <sone.freestyle7898@gmail.com>
2021-08-07 16:19:07 +0700

    2:59 create queue get numeric body

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from linked_list import *
 from hash_table import * 
 from binary_search_tree import *
 import random
+from custom_q import *
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb_user_blog.file"
@@ -134,7 +135,7 @@ def create_blog_post(user_id):
     return jsonify({"message": "new blog post created"}), 200
 
 @app.route("/blog_post/<blog_post_id>", methods=["GET"])
-def get_all_blog_posts(blog_post_id):
+def get_one_blog_post(blog_post_id):
     blog_posts = BlogPost.query.all()
     random.shuffle(blog_posts)
     bst = BinarySearchTree()
@@ -153,9 +154,26 @@ def get_all_blog_posts(blog_post_id):
         return jsonify({"message": "post not found"}), 400
     return jsonify(post), 200
 
-@app.route("/blog_post/<blog_post_id>", methods=["GET"])
-def get_one_blog_post(blog_post_id):
-    pass
+@app.route("/blog_post/numeric_body", methods=["GET"])
+def get_numeric_post_bodies():
+    blog_posts = BlogPost.query.all()
+    q = Queue()
+    for post in blog_posts:
+        q.enqueue(post)
+    return_list = []
+    for _ in range(len(blog_posts)):
+        post = q.dequeue()
+        numeric_body = 0
+        for char in post.data.body:
+            numeric_body += ord(char)
+        post.data.body = numeric_body
+        return_list.append({
+            "id": post.data.id,
+            "title": post.data.title,
+            "body": post.data.body,
+            "user_id": post.data.user_id,
+            })
+    return jsonify(return_list)
 
 @app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
 def delete_blog_post(blog_post_id):
